7e8921dfb39a4a54a598f59355a754d18e16ebf0
vibhorverma24 <123531914+vibhorverma24@users.noreply.github.com>
2023-03-14 22:14:51 +0530

    Add files via upload

---
+++ 04_increment.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int i = 5;
+    printf("the value after i++ is %d\n",i++);
+    printf("the value of i is %d",i);
+    // i++ --> phele print fir increment
+    // ++i --> phele increment fir print
+    return 0;
+}
