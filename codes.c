a05043c0cf34d07d4d5bffd835212d3b21f9a00f
vibhorverma24 <123531914+vibhorverma24@users.noreply.github.com>
2023-03-14 22:25:32 +0530

    Add files via upload

---
+++ codes.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    printf("Vibhor Verma\n");
+    printf("2210992522\n");
+    printf("Enter character");
+    scanf("%c",&ch);
+    printf("the character is %c",&ch);
+    return 0;
+}
