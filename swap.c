ed079962f730ba510055a1077504909abc9ad03b
aryanandoria <122526574+aryanandoria@users.noreply.github.com>
2023-03-14 12:43:45 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,35 @@
+// #include<stdio.h>
+
+// int main() {
+//    int x, y, temp;
+//    printf("Enter the value of x and y: ");
+//    scanf("%d %d", &x, &y);
+//    printf("Before swapping the numbers x=%d, y=%d ", x, y);
+//    temp = x;
+//    x=y;
+//    y=temp;
+//    printf("After swapping the numbers x=%d, y=%d", x, y);
+//    return 0; 
+// }
+
+
+
+
+// #include<stdio.h>
+
+// int main()
+// {
+//     int x, y;
+//     printf("Enter the value of x and y: ");
+//     scanf("%d %d", &x, &y);
+//     printf("Before swapping the numbers x=%d, y=%d ", x, y);
+//     x=x+y;
+//     y=x-y;
+//     x=x-y;
+//     printf("After swapping the numbers x=%d, y=%d", x, y);
+//     return 0; 
+// }
+
+
+
+
